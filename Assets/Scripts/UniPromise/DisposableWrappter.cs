5ab6affa04f8aed57b2a894afb69fb8a26613000
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-07-31 18:36:57 +0900

    PromiseDisposable

---
+++ Assets/Scripts/UniPromise/DisposableWrappter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UniPromise {
+
+	public class DisposableWrappter : IDisposable {
+		Action dispose;
+		
+		public DisposableWrappter (Action dispose) {
+			this.dispose = dispose;
+		}
+		
+		public void Dispose () {
+			dispose();
+		}
+	}
+
+}
---
+++ Assets/Scripts/UniPromise/PromiseDisposable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UniPromise {
+
+	public static class DisposablePromiseExtension {
+		public static IDisposable AsDisposable<T>(this Promise<T> promise, Func<T, IDisposable> converter) {
+			var disposablePromise = promise.Select(converter);
+			return disposablePromise.AsDisposable();
+		}
+
+		public static IDisposable AsDisposable(this Promise<IDisposable> promise) {
+			return new DisposableWrappter(() => promise.Done(disp => disp.Dispose()));
+		}
+	}
+
+}

1bf0f479c58ed12ab88b3631b2990f529aee249b
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-09-17 15:53:50 +0900

    AsyncTask

---
+++ Assets/Scripts/UniPromise/AsyncTask.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace UniPromise {
+	public class AsyncTask<R> {
+		Func<Promise<R>> func;
+		Deferred<R> result;
+		bool started;
+
+		public AsyncTask (Func<Promise<R>> func) {
+			this.func = func;
+			result = new Deferred<R>();
+		}
+
+		public Promise<R> Execute() {
+			if(started)
+				return result;
+
+			func()
+				.Done(r => this.result.Resolve(r))
+					.Fail(e => this.result.Reject(e));
+			return result;
+		}
+
+		public Promise<R> Result {
+			get {
+				return result;
+			}
+		}
+	}
+}

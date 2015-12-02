dd928e83ad00eceeea3da00f238c105742912d5b
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-12-02 16:43:08 +0900

    refactor

---
+++ Assets/Scripts/UniPromise/Internal/AllPromiseFactory.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+
+namespace UniPromise.Internal {
+	internal class AllPromiseFactory<T> {
+		List<Promise<T>> promises;
+		int resolvedCount;
+		int size;
+		T[] result;
+		Deferred<T[]> deferred;
+		
+		public Promise<T[]> Create (List<Promise<T>> promises) {
+			this.promises = promises;
+			resolvedCount = 0;
+			
+			if(promises.Count == 0)
+				return Promises.Resolved(new T[0]);
+			
+			size = promises.Count;
+			deferred = new Deferred<T[]>();
+			
+			result = new T[size];
+			for(int i = 0; i < size; i++){
+				ObservePromise(i);
+			};
+			return deferred;
+		}
+		
+		void ObservePromise(int index){
+			var each = promises[index];
+			each.Done(t => {
+				result[index] = t;
+				resolvedCount++;
+				if(resolvedCount == size)
+					deferred.Resolve(result);
+			})
+				.Fail(e => deferred.Reject(e));
+		}
+	}
+}
--- Assets/Scripts/UniPromise/Promises.cs
+++ Assets/Scripts/UniPromise/Promises.cs
@@ -1,6 +1,5 @@
-using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
+using UniPromise.Internal;
 
 namespace UniPromise {
 
@@ -37,42 +36,4 @@ namespace UniPromise {
 			return new DisposedPromise<T>();
 		}
 	}
-
-
-	public class AllPromiseFactory<T> {
-		List<Promise<T>> promises;
-		int resolvedCount;
-		int size;
-		T[] result;
-		Deferred<T[]> deferred;
-
-		public Promise<T[]> Create (List<Promise<T>> promises) {
-			this.promises = promises;
-			resolvedCount = 0;
-
-			if(promises.Count == 0)
-				return Promises.Resolved(new T[0]);
-			
-			size = promises.Count;
-			deferred = new Deferred<T[]>();
-
-			result = new T[size];
-			for(int i = 0; i < size; i++){
-				ObservePromise(i);
-			};
-			return deferred;
-		}
-
-		void ObservePromise(int index){
-			var each = promises[index];
-			each.Done(t => {
-				result[index] = t;
-				resolvedCount++;
-				if(resolvedCount == size)
-					deferred.Resolve(result);
-			})
-				.Fail(e => deferred.Reject(e));
-		}
-	}
-
 }

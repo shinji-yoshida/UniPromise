5f753a7dc95e6dfe241d74bfc208ef844b32e485
Shinji Yoshida <yoshida.shinji.gm@gmail.com>
2015-12-02 16:54:29 +0900

    refactor

--- Assets/Scripts/UniPromise/Promises.cs
+++ Assets/Scripts/UniPromise/Promises.cs
@@ -4,18 +4,6 @@ using UniPromise.Internal;
 namespace UniPromise {
 
 	public static class Promises {
-		public static Promise<T[]> All<T>(params Promise<T>[] promises) {
-			return All(new List<Promise<T>>(promises));
-		}
-		
-		public static Promise<T[]> All<T>(this IEnumerable<Promise<T>> promises) {
-			return All(new List<Promise<T>>(promises));
-		}
-		
-		public static Promise<T[]> All<T>(this List<Promise<T>> promises) {
-			return new AllPromiseFactory<T>().Create(promises);
-		}
-
 		public static Promise<T[]> AllDone<T>(params Promise<T>[] promises) {
 			return AllDone(new List<Promise<T>>(promises));
 		}

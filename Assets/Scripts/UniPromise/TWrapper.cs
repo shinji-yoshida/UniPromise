5aeb8e6c15acff97cf8ea074e3064b51dbf472ce
Shinji Yoshida <yoshida@nurve.jp>
2016-12-22 12:07:51 +0900

    TWrapper

---
+++ Assets/Scripts/UniPromise/TWrapper.cs
@@ -0,0 +1,17 @@
+﻿
+namespace UniPromise {
+	/// <summary>
+	/// Wrap struct by this class in order to avoid AOT exception.
+	/// </summary>
+	public class TWrapper<T> {
+		public readonly T val;
+
+		public TWrapper (T val) {
+			this.val = val;
+		}
+
+		public static TWrapper<T> Create<T>(T val) {
+			return new TWrapper<T> (val);
+		}
+	}
+}

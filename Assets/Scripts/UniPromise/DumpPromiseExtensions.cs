f9d0c7b596bb1941e9d663e9fea2a4fc9cd5c120
Shinji Yoshida <yoshida@nurve.jp>
2017-06-12 15:08:18 +0900

    AddTo, Dump extensions

---
+++ Assets/Scripts/UniPromise/DumpPromiseExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniPromise {
+	public static class DumpPromiseExtensions {
+		public static Promise<T> Dump<T>(this Promise<T> promise, string name) {
+			promise
+				.Done (i => Debug.Log (string.Format ("{0}-->{1}", name, i)))
+				.Fail (ex => Debug.Log (string.Format ("{0} failed-->{1}", name, ex.Message)))
+				.Disposed (() => Debug.Log (string.Format ("{0} disposed", name)));
+			return promise;
+		}
+	}
+}
